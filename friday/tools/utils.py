9f7544e0f366846f8aea7ba5fb39412355301945
vivekvx <vivek18q@gmail.com>
2026-02-13 18:00:00 +0530

    refactor: simplify utils.py helpers

--- friday/tools/utils.py
+++ friday/tools/utils.py
@@ -39,3 +39,4 @@ def register(mcp):
 # docs: improve utils.py documentation at 2026-01-22T10:00:00+05:30
 # refactor: simplify utils.py helpers at 2026-01-29T18:00:00+05:30
 # docs: improve utils.py documentation at 2026-02-06T10:00:00+05:30
+# refactor: simplify utils.py helpers at 2026-02-13T18:00:00+05:30
