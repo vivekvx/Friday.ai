897e2587b6557efea314b66f74f47ad7936ac408
vivekvx <vivek18q@gmail.com>
2026-04-18 10:00:00 +0530

    feat: add context to __init__.py

--- friday/__init__.py
+++ friday/__init__.py
@@ -11,3 +11,4 @@
 # feat: refine __init__.py structure at 2026-03-26T18:00:00+05:30
 # feat: add context to __init__.py at 2026-04-03T10:00:00+05:30
 # feat: refine __init__.py structure at 2026-04-10T18:00:00+05:30
+# feat: add context to __init__.py at 2026-04-18T10:00:00+05:30
