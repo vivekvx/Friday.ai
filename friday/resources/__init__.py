6f8b4d3331680ec055e978a5562ecca702c58402
vivekvx <vivek18q@gmail.com>
2026-04-27 18:00:00 +0530

    chore: polish __init__.py content

--- friday/resources/__init__.py
+++ friday/resources/__init__.py
@@ -20,3 +20,4 @@ def register_all_resources(mcp):
 # chore: finalize __init__.py notes at 2026-04-05T10:00:00+05:30
 # chore: polish __init__.py content at 2026-04-12T18:00:00+05:30
 # chore: finalize __init__.py notes at 2026-04-20T10:00:00+05:30
+# chore: polish __init__.py content at 2026-04-27T18:00:00+05:30
